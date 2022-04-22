4d715e77b5fa141275f3403c2bfb2ca885b8bc30
Rodge3 <muthumohan0305@gmail.com>
2022-04-22 15:33:06 +0530

    Commit this

---
+++ UserdetailsManagement1-2/src/main/java/com/example/demo/UserdetailsManagement12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdetailsManagement12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdetailsManagement12Application.class, args);
+	}
+
+}
